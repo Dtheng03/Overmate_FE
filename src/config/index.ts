1ffa46312d29708529a065c268a99c12753ce3af
Nguyen Duc Thang <aloducthang@gmail.com>
2024-10-08 16:16:05 +0700

    deploy

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: "https://localhost:5001/api/",
+    // baseURL: "https://localhost:5001/api/",
+    baseURL: "https://overmaterepo-latest.onrender.com/api/",
     headers: {
         'Content-type': 'application/json',
     },
